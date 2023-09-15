08834ff2e92c3b5ca89afea74e78e25b56579536
9000HAL <gsanchezscs@gmail.com>
2023-09-14 20:45:52 -0700

    restart-- new documentation --- good here renders

--- app.py
+++ app.py
@@ -1,59 +1,20 @@
-from flask import Flask, render_template
-# import config
-#v.2 above ^^^^^^^^^^^^^^^^^^^^^^see above config SGUIGGGLY --- !!!!!!!!!!!!!!!!!!!!!!
-
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'postgres://habrpuql:PggYGrzmBoFFYntRSRSMstXjCYm6mmxi@bubble.db.elephantsql.com/habrpuql'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://habrpuql:PggYGrzmBoFFYntRSRSMstXjCYm6mmxi@bubble.db.elephantsql.com/habrpuql'
 
 db = SQLAlchemy(app)
 
 
 
-#v.2 do next ----------------------
-
-# Initialize Flask app
-app = Flask(__name__)
-app.config.from_object('config')
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# v.1 to delete next ---------------------- renders SUCCESSFULLY
-
-""" 
-from flask import Flask
-app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, LB!'
 
 if __name__ == '__main__':
     app.run()
-"""
-# v.1 to delete next ----------------------
-
-

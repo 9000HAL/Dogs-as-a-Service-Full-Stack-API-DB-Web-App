06a6cb642a38ee6697ba641d7317ef6f64c5c646
9000HAL <gsanchezscs@gmail.com>
2023-09-15 13:17:02 -0700

    resuming `part 3` friday am re-open

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate  # Make sure to import this
-
+import requests
 import config
+from flask import render_template, request, redirect, url_for, flash #documentation reccomend....?
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
 
 
 
 app = Flask(__name__)
 app.config.from_object(config)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://habrpuql:PggYGrzmBoFFYntRSRSMstXjCYm6mmxi@bubble.db.elephantsql.com/habrpuql'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://habrpuql:PggYGrzmBoFFYntRSRSMstXjCYm6mmxi@bubble.db.elephantsql.com/habrpuql' = NO WORKS DO NOT USE 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-
 
 
 
@@ -26,14 +28,13 @@ class User(db.Model):
 
 
 
-
-
 @app.route('/')
 def hello_world():
     return 'Hello, LB!'
 
 if __name__ == '__main__':
     app.run()
+
 
 
 @app.route('/add_user/<username>/<email>', methods=['GET'])
@@ -44,10 +45,12 @@ def add_user(username, email):
     return f'User {new_user.username} added.'
 
 
+
 @app.route('/users', methods=['GET'])
 def list_users():
     users = User.query.all()
     return '<br>'.join([f'User: {user.username}, Email: {user.email}' for user in users])
+
 
 
 @app.route('/update/<username>/<new_email>', methods=['GET'])
@@ -61,6 +64,7 @@ def update_user(username, new_email):
         return f'User {username} does not exist.'
 
 
+
 @app.route('/delete/<username>', methods=['GET'])
 def delete_user(username):
     user = User.query.filter_by(username=username).first()
@@ -70,3 +74,11 @@ def delete_user(username):
         return f'User {username} deleted.'
     else:
         return f'User {username} does not exist.'
+
+
+
+@app.route('/random_dog', methods=['GET'])
+def random_dog():
+    response = requests.get('https://dog.ceo/api/breeds/image/random')
+    data = response.json()
+    return f"<img src='{data['message']}'>"
